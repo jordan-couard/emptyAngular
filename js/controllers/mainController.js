a92110c703d56eddb3007dd5457fc20e33fa9c64
jordan-couard <jordan.couard@gmail.com>
2017-03-21 17:22:01 +0100

    fin de quete

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,24 @@
 // MAIN CONTROLLER
-function mainController($scope) {
-    
-}
+angular.module('app')
+    .controller('mainController', function($scope) {
+        $scope.tabs = [];
+        var ligne;
+
+        $scope.firstName = '';
+        $scope.lastName = '';
+        $scope.mail = '';
+
+
+        $scope.add = function() {
+            console.log('coucou');
+            ligne = {
+                firstName: $scope.firstName,
+                lastName: $scope.lastName,
+                mail: $scope.mail
+            };
+
+
+            $scope.tabs.push(ligne);
+            console.log($scope.tabs);
+        };
+    });
